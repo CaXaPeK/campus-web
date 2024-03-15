88ce10947b5e88a1cd300019db53dfafd323a8bd
Саша Баздырев <sasha.bazdyrev2004@gmail.com>
2024-03-15 16:16:33 +0700

    Working routing & something like a navbar

---
+++ campus/src/constants/routes.js
@@ -0,0 +1,11 @@
+export const ROUTES = {
+    ROOT: '/',
+    LOGIN: 'login',
+    REGISTRATION: 'registration',
+    PROFILE: 'profile',
+    GROPUS: 'groups',
+    COURSES: 'groups/:groupId',
+    COURSE: 'courses/:courseId',
+    MY_COURSES: 'courses/my',
+    TEACHING_COURSES: 'courses/teaching'
+};
---
+++ campus/src/routes/root.jsx
@@ -0,0 +1,12 @@
+import { Outlet } from 'react-router';
+import Navbar from '../components/navbar/Navbar';
+
+export const Root = () => {
+  return (
+    <>
+      <Navbar />
+      <Outlet />
+    </>
+  );
+}
+
